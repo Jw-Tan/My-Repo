6e64eb95279ea5b1498deeefe7b96bfc41b19485
Jw-Tan <jwtan@hotmail.sg>
2016-01-25 10:46:40 +0800

    Revert "Added change() function"
    This reverts commit 04e038740e170dd79e4a5f05194b48512d6098ca.

--- Test.java
+++ Test.java
@@ -1,10 +1,6 @@
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println(change("Hello World!"));		
-	}
-
-	public static String change(String str) {
-		return str + str;
+		System.out.println("Hello World!");		
 	}
 }
